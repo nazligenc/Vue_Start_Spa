669c597e260d34755243e9ffe6815636469ce7b6
nazligenc <nazli.gencx@gmail.com>
2025-05-16 01:12:28 +0300

    using the created() lifecycle event to load data

--- src/App.vue
+++ src/App.vue
@@ -10,26 +10,24 @@ import Navbar from './components/Navbar.vue';
             PageViewer,
             Navbar
         },
+        created(){
+            this.getPages();
+
+        },
         data(){
             return{
                 activePage:0,
-                pages:[{
-                    link:{text:'Home',url:'home.html'},
-                    pageTitle:'Home Page',
-                    content:'This is the Home content'
-                },
-                {
-                    link:{text:'About',url:'about.html'},
-                    pageTitle:'About Page',
-                    content:'This is the About content'
-                },
-                {
-                    link:{text:'Contact',url:'contact.html'},
-                    pageTitle:'Contact Page',
-                    content:'This is the Contact content'
-                }
-                ]
+                pages:this.getPages()
+               
             }
+        },
+        methods:{
+            async getPages(){
+                let res=await fetch('pages.json');
+                let data= await res.json();
+                this.pages=data;
+            }
+
         }
     }
 </script>
